69a0802d1a7441e96061e8ce166c0c426ea668e8
fran-byte <fran-byte@hotmail.com>
2025-12-21 22:47:07 +0100

    up

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,22 +6,25 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 20:52:56 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/21 21:21:57 by frromero         ###   ########.fr       */
+/*   Updated: 2025/12/21 22:46:58 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 #include <cstddef>
+#include <iostream>
 
+template <typename T>
 void Func(T array)
 {
+    std::cout << array << std::endl;
 }
 
 template <typename T, typename Func>
 void inter(T *arrayAddr, size_t size, Func function)
 {
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
         function(arrayAddr[i]);
 };
 
