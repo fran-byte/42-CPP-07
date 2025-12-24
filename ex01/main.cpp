8163faa098757e8cc2c840e243d9d17960f88c7c
fran-byte <fran-byte@hotmail.com>
2025-12-24 13:42:46 +0100

    up

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 22:47:55 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/21 23:17:24 by frromero         ###   ########.fr       */
+/*   Updated: 2025/12/24 13:42:40 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int main(void)
 {
     int array[] = {100, 200, 300, 400, 500, 600};
-    ::iter(array, 6, ::printed<int>); /*pass by No const*/
+    ::iter(array, 6, ::printed<int>); /*pass by NO const,
+                            It is necessary to specify the argument type <int>*/
 
     const char *arrayStr[] = {
         "one hundred",
