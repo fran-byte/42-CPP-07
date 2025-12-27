0cfdbe023caef994dd8ee8a4aec6a4bbeedae8ff
fran-byte <fran-byte@hotmail.com>
2025-12-27 18:11:39 +0100

    up

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 22:47:55 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/24 13:42:40 by frromero         ###   ########.fr       */
+/*   Updated: 2025/12/27 18:11:30 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int main(void)
 {
     int array[] = {100, 200, 300, 400, 500, 600};
+
     ::iter(array, 6, ::printed<int>); /*pass by NO const,
                             It is necessary to specify the argument type <int>*/
 
@@ -25,6 +26,7 @@ int main(void)
         "four hundred",
         "five hundred",
         "six hundred"};
+
     ::iter(arrayStr, 6, ::printed<const char *>); /*pass by const reference*/
 
     return 0;
